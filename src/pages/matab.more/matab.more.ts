2dff961a77a526333d35a36b6e1bab82d293c8a8
ghermezkon@gmail.com <89772@bmi-khz.local>
2018-07-26 12:55:47 +0430

    970504

--- src/pages/matab.more/matab.more.ts
+++ src/pages/matab.more/matab.more.ts
@@ -1,138 +1,124 @@
 import { Component } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { FormControl, Validators, FormGroup, FormBuilder } from "@angular/forms";
 import { Subscription, Observable } from "rxjs";
-import { FormBuilder } from "@angular/forms";
 import { HttpService } from "../../http/HttpService";
 import { MessageUtil } from "../../util/message.util";
 import { SelectSearchableComponent } from "ionic-select-searchable";
 import { InfiniteScroll } from "ionic-angular";
-import { delay } from "rxjs/operators";
+import { delay, take } from "rxjs/operators";
 import { Matab } from "../../util/global.class";
-import { FormControl } from "@angular/forms";
-import { Validators } from "@angular/forms";
 
+import * as _ from 'lodash';
 @Component({
     selector: 'matab-more-page',
     templateUrl: 'matab.more.html'
 })
 export class MatabMorePage {
     dataForm: FormGroup;
-    matab_list: any[] = [];
+    data_list: any[] = [];
     counter_list: any[] = [];
     perPage = 0;
     totalData = 0;
     totalPage = 0;
-
+    page = 1;
     validation_msg: any;
-    idx: any = -1;
     //----------------------------------------------------------
-    page = 2;
-    doctorsSubscription: Subscription;
-    current_matab: any;
+    current_matab: any = null;
+    matab_name: any = null;
     days: any[] = [];
+    dayOptions = {
+        title: 'روزهای هفته',
+        subTitle: 'انتخاب روزهای تعطیل',
+        mode: 'md'
+    };
     //------------------------------------------------------
     constructor(public fb: FormBuilder, public _http: HttpService, public _msg: MessageUtil) { }
     //------------------------------------------------------
     ionViewWillLoad() {
         this.days = this._msg.days;
+        this.validation_msg = this._msg.validate_msg();
         this.createForm();
-        this._http.getAll('matab_base').subscribe((_: any) => {
-            this.matab_list = _;
+        this._http.getAll('matab').subscribe((_: any) => {
+            if (_.length > 11) {
+                this.counter_list = _;
+                for (let i = 0; i < 11; i++) {
+                    this.data_list.push(_[i]);
+                }
+                this.perPage = 10;
+                this.totalData = _.length;
+                this.totalPage = Math.ceil(this.totalData / this.perPage);
+            } else {
+                this.data_list = _;
+            }
         });
     }
     //------------------------------------------------------
-    getMoreMatabs(event: { component: SelectSearchableComponent, infiniteScroll: InfiniteScroll, text: string }) {
-        let text = (event.text || '').trim();
-        if (this.page > 3) {
-            event.infiniteScroll.enable(false);
-            return;
-        }
-        this.getMatabsAsync(this.page, 15).subscribe(res => {
-            res = event.component.items.concat(res);
-            if (text) {
-                res = this.filterMatabs(res, text);
-            }
-            event.component.items = res;
-            event.infiniteScroll.complete();
-            this.page++;
-        });
-    }
-    //---------------------------------------------------------- 
-    getMatabsAsync(page?: number, size?: number, timeout = 100): Observable<any[]> {
-        return new Observable<any[]>(observer => {
-            observer.next(this.getMatabs(page, size));
-            observer.complete();
-        }).pipe(delay(timeout));
-    }
-    //---------------------------------------------------------- 
-    getMatabs(page?: number, size?: number): any[] {
-        let matabs = [];
-        this.matab_list.forEach(matab => {
-            matabs.push(matab);
-        });
-        if (page && size) {
-            matabs = matabs.slice((page - 1) * size, ((page - 1) * size) + size);
-        }
-        return matabs;
-    }
-    //---------------------------------------------------------- 
-    filterMatabs(matabs: any[], text: string) {
-        return matabs.filter(matab => {
-            return matab.matab_base.matab_doctor.doctor_name.indexOf(text) !== -1;
-        });
-    }
-    //---------------------------------------------------------- 
-    searchMatabsInfinite(event: { component: SelectSearchableComponent, infiniteScroll: InfiniteScroll, text: string }) {
-        let text = event.text.trim();
-        event.component.startSearch();
-        if (this.doctorsSubscription) {
-            this.doctorsSubscription.unsubscribe();
-        }
-        if (!text) {
-            if (this.doctorsSubscription) {
-                this.doctorsSubscription.unsubscribe();
-            }
-            event.component.items = this.getMatabs(1, 15);
-            this.page = 2;
-            event.component.endSearch();
-            if (event.infiniteScroll) {
-                event.infiniteScroll.enable(true);
-            }
-            return;
-        }
-        this.doctorsSubscription = this.getMatabsAsync().subscribe(ports => {
-            if (this.doctorsSubscription.closed) {
-                return;
-            }
-            event.component.items = this.filterMatabs(ports, text);
-            event.component.endSearch();
-        });
-    }
-    //---------------------------------------------------------- 
     createForm() {
         this.dataForm = this.fb.group({
             matab_start_time: ['', Validators.required],
             matab_end_time: ['', Validators.required],
             matab_number_bed: ['', Validators.compose([Validators.required, Validators.pattern('[0-9]*')])],
-            matab_close_day: ['', Validators.required],
+            matab_close_day: [''],
             matab_date_close_start: [''],
             matab_date_close_end: [''],
             matab_is_enable: [true],
         });
     }
+    //------------------------------------------------------
+    compareFn(e1: any, e2: any): boolean {
+        return e1 && e2 ? e1.day_value === e2.day_value : e1 === e2;
+    }
+    //------------------------------------------------------
+    set_data(data, idx) {
+        this.dataForm.patchValue({
+            matab_start_time: data.matab_more.matab_start_time,
+            matab_end_time: data.matab_more.matab_end_time,
+            matab_number_bed: data.matab_more.matab_number_bed,
+            matab_close_day: data.matab_more.matab_close_day,
+            matab_date_close_start: data.matab_more.matab_date_close_start,
+            matab_date_close_end: data.matab_more.matab_date_close_end,
+            matab_is_enable: data.matab_more.matab_is_enable
+        });
+        this.current_matab = data;
+        this.matab_name = data.matab_base.matab_doctor.doctor_name + "(" + data.matab_base.matab_doctor.cd.cd_name + ") - " +
+            data.matab_base.matab_city.city_name;
+        return true;
+    }
+    //------------------------------------------------------
+    edit(data) {
+        let matab_form: FormGroup;
+        matab_form = this.fb.group({ ...Matab });
+        matab_form.get('_id').setValue(this.current_matab._id);
+        matab_form.get('matab_base').setValue(this.current_matab.matab_base);
+        matab_form.get('matab_more').setValue(data);
+
+        let list = [...this.data_list];
+
+        this._http.update(matab_form.value).pipe(take(1)).subscribe((json: any) => {
+            if (json.nModified >= 1) {
+                this._msg.showMessage('update_ok');
+
+                let index = _.findIndex(this.data_list, function (o) { return o._id == data._id; });
+                list[index] = data;
+                this.data_list = list;
+            } else {
+                this._msg.showMessage('update_error');
+            }
+        });
+    }
     //---------------------------------------------------------- 
-    // doInfinite(infiniteScroll) {
-    //     this.totalPage = this.page * 10;
-    //     setTimeout(() => {
-    //         let result = this.counter_list.slice(this.page * 10);
-    //         for (let i = 1; i < this.perPage; i++) {
-    //             if (result[i] != undefined) {
-    //                 this.data_list.push(result[i])
-    //             }
-    //         }
-    //         this.page += 1;
-    //         infiniteScroll.complete();
-    //     }, 500);
-    // }
-    //---------------------------------------------------------- 
+    doInfinite(infiniteScroll) {
+        this.totalPage = this.page * 10;
+        setTimeout(() => {
+            let result = this.counter_list.slice(this.page * 10);
+            for (let i = 1; i < this.perPage; i++) {
+                if (result[i] != undefined) {
+                    this.data_list.push(result[i])
+                }
+            }
+            this.page += 1;
+            infiniteScroll.complete();
+        }, 500);
+    }
+    //----------------------------------------------------------     
 }
